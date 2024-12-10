f23bdba841b15491e2b07d505c34a65b7cf40717
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-12-10 22:31:26 +0900

    Update TokenProvider.java

--- Microservice/BE-AUTH/src/main/java/com/example/msaauth/jwt/TokenProvider.java
+++ Microservice/BE-AUTH/src/main/java/com/example/msaauth/jwt/TokenProvider.java
@@ -33,7 +33,7 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 2000;           // 1000분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 2000;           // 100분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
     private static final String MEMBER_ID_KEY = "memberId";
 
